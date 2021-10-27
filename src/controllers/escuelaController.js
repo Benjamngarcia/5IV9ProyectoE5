60cb78b2f8c9629d34b6c6431b35f23a324dd227
Benjamín Arturo Pérez García <bperezg1800@alumno.ipn.mx>
2021-10-26 20:47:19 -0500

    links landing page

--- src/controllers/escuelaController.js
+++ src/controllers/escuelaController.js
@@ -1,8 +1,22 @@
 const controller = {};
 const pool = require('../database');
 
+//Abrir página inicial
 controller.open = (req, res) => {
     res.render('index');
+}
+//Links a dropdown menu
+controller.LogAlumn = (req, res) => {
+    res.render('logalumn');
+}
+controller.LogTeacher = (req, res) => {
+    res.render('logteacher');
+}
+controller.LogDirec = (req, res) => {
+    res.render('logdirec');
+}
+controller.LogAdmin = (req, res) => {
+    res.render('logadmin');
 }
 
 module.exports = controller;
--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const escuelaController = require('../controllers/escuelaController');
 
 router.get('/', escuelaController.open);
+router.get('/LogAlumn', escuelaController.LogAlumn);
+router.get('/LogTeacher', escuelaController.LogTeacher);
+router.get('/LogDirec', escuelaController.LogDirec);
+router.get('/LogAdmin', escuelaController.LogAdmin);
 
 module.exports = router;
