62a9d8d77c655d3eeb41f44b515afcf3e0cc2398
Benjamín Arturo Pérez García <bperezg1800@alumno.ipn.mx>
2021-11-09 09:57:18 -0600

    comando para iniciar heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const pool = require('../database');
+// const express = require('express');
+// const router = express.Router();
+// const pool = require('../database');
 
 //---------ACCIONES ALUMNO DESDE VISTA DIRECTOR-------------
 //ELIMINAR ALUMNO
-router.get('/DeleteAlum/:id', async(req, res) =>{
-    const { id } = req.params;
-    await pool.query('DELETE FROM alumno WHERE id_alum = ?', [id]);
-    res.redirect('/escuela/VistaDirec');
-});
+// router.get('/DeleteAlum/:id', async(req, res) =>{
+//     const { id } = req.params;
+//     await pool.query('DELETE FROM alumno WHERE id_alum = ?', [id]);
+//     res.redirect('/escuela/VistaDirec');
+// });
 //EDITAR ALUMNO
-router.get('/EditAlum/:id', async (req,res) =>{
-    const { id } = req.params;
-    const alumno = await pool.query('SELECT * FROM alumno WHERE id_alum = ?',[id], (err, rows)=>{
-        res.render('editalum', {
-            data: rows[0]
-        });
-    });
-});
-router.post('/EditAlum/:id', async (req,res) =>{
-    const { id } = req.params;
-    const newInfo = req.body;
-    await pool.query('UPDATE alumno set ? WHERE id_alum = ?', [newInfo, id]);
-    res.redirect('/escuela/VistaDirec');
-});
+// router.get('/EditAlum/:id', async (req,res) =>{
+//     const { id } = req.params;
+//     const alumno = await pool.query('SELECT * FROM alumno WHERE id_alum = ?',[id], (err, rows)=>{
+//         res.render('editalum', {
+//             data: rows[0]
+//         });
+//     });
+// });
+// router.post('/EditAlum/:id', async (req,res) =>{
+//     const { id } = req.params;
+//     const newInfo = req.body;
+//     await pool.query('UPDATE alumno set ? WHERE id_alum = ?', [newInfo, id]);
+//     res.redirect('/escuela/VistaDirec');
+// });
 
-module.exports = router;
+// module.exports = router;
